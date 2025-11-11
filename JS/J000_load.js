60b7b8c33f6d50a3fe5f49a7f03faa5559f212f9
WilliamDava <williamdava87@gmail.com>
2025-11-11 12:40:22 +0100

    Primera prueba de setear cosas

---
+++ JS/J000_load.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function () {
+
+    /* const nodedificultyEasy = document.querySelector(".dificulty__select--easy");
+    nodedificultyEasy.addEventListener($setdificultyToEasy) 
+    AQUI EN EL FUTURO PONDREMOS COSAS ESTATICAS
+    */
+
+})
+
+
---
+++ JS/J001_events.js
@@ -0,0 +1,14 @@
+import j002 from './j002_getData.js';
+
+const j001_events = {
+    defEvent(functionality, typeEvent) {
+        switch (functionality) {
+            case "easy":
+                /* const nodedificultyEasy = document.querySelector(".dificulty__select--easy");
+                nodedificultyEasy.addEventListener($setdificultyToEasy) */
+                console.log("Easy")
+                break;
+        }
+    }
+}
+export default j001_events;
